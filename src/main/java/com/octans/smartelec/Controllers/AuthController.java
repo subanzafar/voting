309d3246cad81cda06c0f89a9ce10fae4834b44f
subhan.zafar <subanmirza4@gmail.com>
2022-05-16 00:47:11 -0700

    updated

--- src/main/java/com/octans/smartelec/Controllers/AuthController.java
+++ src/main/java/com/octans/smartelec/Controllers/AuthController.java
@@ -8,11 +8,8 @@ import com.octans.smartelec.Repositories.ElectionRepository;
 import com.octans.smartelec.Repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
